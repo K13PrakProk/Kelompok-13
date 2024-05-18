05edf0e6d7eaf18bbb1b5d1d22f8049669118d09
unknown <harisainurrofiq01@gmail.com>
2024-05-18 11:56:36 +0700

    Kelompok 13

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def halaman_login():
     password = tk.Entry(window1, width=25, fg="white", border=4, bg="black", font=(11), show="*")
     password.place(x=750, y=380)
 
-    img = Image.open("logo1.png")
+    img = Image.open("Kelompok-13\logo1.png")
     img = ImageTk.PhotoImage(img)
     label = tk.Label(window1, image=img, bd=0, highlightthickness=0)
     label.place(x=105, y=55)
@@ -53,7 +53,7 @@ def halaman_login():
         password = tk.Entry(window2, width=25, fg="white", border=4, bg="black", font=(11), show="*")
         password.place(x=750, y=380)
 
-        img = Image.open("logo1.png")
+        img = Image.open("Kelompok-13\logo1.png")
         img = ImageTk.PhotoImage(img)
         label = tk.Label(window2, image=img, bd=0, highlightthickness=0)
         label.place(x=105, y=55)
@@ -70,6 +70,7 @@ def halaman_login():
 
         window2.mainloop()
 
+    
     def halaman_utama():
         window1.destroy()
         window3 = tk.Tk()
@@ -78,45 +79,53 @@ def halaman_login():
         window3.configure(bg="black")
         window3.resizable(True, True)
 
-        frame = Frame(window3, width=150, height=225, bg="white")
-        frame.place(x=200,y=100)
-        Button(frame, width=20, height=2, text="MCD", fg="black", bg="yellow").place(x=0,y=185)
-        img = Image.open("mcd.jpg")
-        img = img.resize((185, 185))
-        img = ImageTk.PhotoImage(img)
-        label = tk.Label(frame, image=img, bd=0, highlightthickness=0)
-        label.image = img
-        label.place(x=-17, y=0)
+        # MCD
+        frame1 = Frame(window3, width=150, height=225, bg="white")
+        frame1.place(x=200, y=100)
+        Button(frame1, width=20, height=2, text="MCD", fg="black", bg="yellow").place(x=0, y=185)
+        img1 = Image.open("Kelompok-13/mcd.jpg")
+        img1 = img1.resize((185, 185))
+        img1 = ImageTk.PhotoImage(img1)
+        label1 = tk.Label(frame1, image=img1, bd=0, highlightthickness=0)
+        label1.image = img1
+        label1.place(x=-17, y=0)
 
-        frame = Frame(window3, width=150, height=225, bg="white")
-        frame.place(x=550,y=100)
-        Button(frame, width=20, height=2, text="KFC", fg="black", bg="yellow").place(x=0,y=185)
-        img = Image.open("kfc.jpg")
-        img = img.resize((185, 185))
-        img = ImageTk.PhotoImage(img)
-        label = tk.Label(frame, image=img, bd=0, highlightthickness=0)
-        label.place(x=-17, y=0)
+        # KFC
+        frame2 = Frame(window3, width=150, height=225, bg="white")
+        frame2.place(x=550, y=100)
+        Button(frame2, width=20, height=2, text="KFC", fg="black", bg="yellow").place(x=0, y=185)
+        img2 = Image.open("Kelompok-13/kfc.jpg")
+        img2 = img2.resize((185, 185))
+        img2 = ImageTk.PhotoImage(img2)
+        label2 = tk.Label(frame2, image=img2, bd=0, highlightthickness=0)
+        label2.image = img2
+        label2.place(x=-17, y=0)
 
-        frame = Frame(window3, width=150, height=225, bg="white")
-        frame.place(x=900,y=100)
-        Button(frame, width=20, height=2, text="A&W", fg="black", bg="yellow").place(x=0,y=185)
-        img = Image.open("aw.png")
-        img = img.resize((150, 100))
-        img = ImageTk.PhotoImage(img)
-        label = tk.Label(frame, image=img, bd=0, highlightthickness=0)
-        label.place(x=0, y=50)
+        # A&W
+        frame3 = Frame(window3, width=150, height=225, bg="white")
+        frame3.place(x=900, y=100)
+        Button(frame3, width=20, height=2, text="A&W", fg="black", bg="yellow").place(x=0, y=185)
+        img3 = Image.open("Kelompok-13/aw.png")
+        img3 = img3.resize((150, 100))
+        img3 = ImageTk.PhotoImage(img3)
+        label3 = tk.Label(frame3, image=img3, bd=0, highlightthickness=0)
+        label3.image = img3
+        label3.place(x=0, y=50)
 
-        frame = Frame(window3, width=150, height=225, bg="white")
-        frame.place(x=200,y=400)
-        Button(frame, width=20, height=2, text="Burger King", fg="black", bg="yellow").place(x=0,y=185)
+        # Burger King
+        frame4 = Frame(window3, width=150, height=225, bg="white")
+        frame4.place(x=200, y=400)
+        Button(frame4, width=20, height=2, text="Burger King", fg="black", bg="yellow").place(x=0, y=185)
 
-        frame = Frame(window3, width=150, height=225, bg="white")
-        frame.place(x=550,y=400)
-        Button(frame, width=20, height=2, text="Yoshinoya", fg="black", bg="yellow").place(x=0,y=185)
+        # Yoshinoya
+        frame5 = Frame(window3, width=150, height=225, bg="white")
+        frame5.place(x=550, y=400)
+        Button(frame5, width=20, height=2, text="Yoshinoya", fg="black", bg="yellow").place(x=0, y=185)
 
-        frame = Frame(window3, width=150, height=225, bg="white")
-        frame.place(x=900,y=400)
-        Button(frame, width=20, height=2, text="Pizza Hut", fg="black", bg="yellow").place(x=0,y=185)
+        # Pizza Hut
+        frame6 = Frame(window3, width=150, height=225, bg="white")
+        frame6.place(x=900, y=400)
+        Button(frame6, width=20, height=2, text="Pizza Hut", fg="black", bg="yellow").place(x=0, y=185)
 
         window3.mainloop()
 
